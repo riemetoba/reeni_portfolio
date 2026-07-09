b08d480edd1293477532afd0aa5681e3c4f2adfa
Toba <riemehasan06@gmail.com>
2026-07-09 23:05:32 +0600

    fix: some style issue

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -12,7 +12,7 @@ const Icons = () => {
         (Icon, index) => (
           <div
             key={index}
-            className="flex items-center justify-center bg-[#0A0A0A]/5 dark:bg-[#1A1A1A] text-common dark:text-white hover:!bg-red hover:!text-white transition-all duration-300 rounded-full h-12 w-12 cursor-pointer shrink-0"
+            className="flex items-center justify-center bg-bg-dark-main/5 dark:bg-[#1A1A1A] text-common dark:text-white hover:!bg-red hover:!text-white transition-all duration-300 rounded-full h-12 w-12 cursor-pointer shrink-0"
           >
             <Icon className="text-lg" />
           </div>
