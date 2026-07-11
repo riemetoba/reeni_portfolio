b8379276b880ff6554be5798b7fc71e305e66e8f
Toba <riemehasan06@gmail.com>
2026-07-11 22:28:08 +0600

    fix: some space & gradient color issue

--- src/components/layouts/ExperienceCounter.jsx
+++ src/components/layouts/ExperienceCounter.jsx
@@ -2,9 +2,10 @@ const ExperienceCounter = () => {
   return (
     <section className="bg-white dark:bg-bg-dark-main transition-colors duration-300">
       <div className="container flex flex-col lg:flex-row lg:justify-between items-center gap-8 py-20 px-5 lg:px-0">
+        
         {/* Left side Card */}
         <div className="lg:w-1/2 w-full">
-          <div className="bg-[#F4F4F4] dark:bg-[#1a1a1a] dark:bg-[radial-gradient(circle_at_100%_100%,rgba(239,68,68,0.5)_0%,rgba(239,68,68,0.2)_25%,transparent_55%)] lg:px-13 px-5 lg:pt-9 pt-7.5 lg:pb-13 pb-7.5 rounded-[20px] overflow-hidden relative transition-colors duration-300">
+          <div className="bg-[#F4F4F4] dark:bg-[#1a1a1a] dark:bg-[radial-gradient(circle_at_100%_100%,rgba(239,68,68,0.2)_0%,rgba(239,68,68,0.2)_25%,transparent_55%)] lg:px-13 px-5 lg:pt-9 pt-7.5 lg:pb-13 pb-7.5 rounded-[20px] overflow-hidden relative transition-colors duration-300">
             <div className="lg:flex gap-11 items-center">
               <h3 className="lg:text-[167px] text-[100px] text-red font-bold font-inter lg:leading-45.75 leading-25">
                 25
@@ -66,7 +67,7 @@ const ExperienceCounter = () => {
                   <h2 className="text-[48px] text-common dark:text-white font-bold font-inter leading-15.5 text-center">
                     1,000+
                   </h2>
-                  <p className="text-base text-[#5D6570] dark:text-[#9F9F9Ffix] font-normal font-rubik leading-6.5 mt-1 text-center">
+                  <p className="text-base text-[#5D6570] dark:text-[#9F9F9F] font-normal font-rubik leading-6.5 mt-1 text-center">
                     our Satisfied Clientd
                   </p>
                 </div>
