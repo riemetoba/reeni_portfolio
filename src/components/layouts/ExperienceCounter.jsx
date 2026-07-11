8c0aaf72314307de0c75ff0a4b834cf226ea412e
Toba <riemehasan06@gmail.com>
2026-07-11 22:20:17 +0600

    fix(experienceCounter): use custom tailwind color name instead of hex code

--- src/components/layouts/ExperienceCounter.jsx
+++ src/components/layouts/ExperienceCounter.jsx
@@ -4,7 +4,7 @@ const ExperienceCounter = () => {
       <div className="container flex flex-col lg:flex-row lg:justify-between items-center gap-8 py-20 px-5 lg:px-0">
         {/* Left side Card */}
         <div className="lg:w-1/2 w-full">
-          <div className="bg-[#F4F4F4] dark:bg-[#1a1a1a] dark:bg-[image:radial-gradient(circle_at_100%_100%,rgba(239,68,68,0.5)_0%,rgba(239,68,68,0.2)_25%,transparent_55%)] lg:px-13 px-5 lg:pt-9 pt-7.5 lg:pb-13 pb-7.5 rounded-[20px] overflow-hidden relative transition-colors duration-300">
+          <div className="bg-[#F4F4F4] dark:bg-[#1a1a1a] dark:bg-[radial-gradient(circle_at_100%_100%,rgba(239,68,68,0.5)_0%,rgba(239,68,68,0.2)_25%,transparent_55%)] lg:px-13 px-5 lg:pt-9 pt-7.5 lg:pb-13 pb-7.5 rounded-[20px] overflow-hidden relative transition-colors duration-300">
             <div className="lg:flex gap-11 items-center">
               <h3 className="lg:text-[167px] text-[100px] text-red font-bold font-inter lg:leading-45.75 leading-25">
                 25
@@ -25,7 +25,7 @@ const ExperienceCounter = () => {
           <div>
             <div className="lg:flex md:flex justify-between items-center gap-8">
               <div className="lg:w-1/2 w-full lg:mt-0 mt-8">
-                <div className="py-9 bg-[#f4f4f4] dark:bg-[#141414] rounded-[20px] border-b-3 border-r-3 border-red dark:border-none transition-colors duration-300">
+                <div className="py-9 bg-[#f4f4f4] dark:bg-common rounded-[20px] border-b-3 border-r-3 border-red dark:border-none transition-colors duration-300">
                   <h2 className="text-[48px] text-common dark:text-white font-bold font-inter leading-15.5 text-center">
                     20k+
                   </h2>
@@ -36,7 +36,7 @@ const ExperienceCounter = () => {
               </div>
 
               <div className="lg:w-1/2 w-full lg:mt-0 mt-8">
-                <div className="py-9 bg-[#f4f4f4] dark:bg-[#141414] rounded-[20px] border-b-3 border-r-3 border-red dark:border-none transition-colors duration-300">
+                <div className="py-9 bg-[#f4f4f4] dark:bg-common rounded-[20px] border-b-3 border-r-3 border-red dark:border-none transition-colors duration-300">
                   <h2 className="text-[48px] text-common dark:text-white font-bold font-inter leading-15.5 text-center">
                     10k+
                   </h2>
@@ -51,7 +51,7 @@ const ExperienceCounter = () => {
           <div>
             <div className="lg:flex md:flex justify-between items-center gap-8 mt-8">
               <div className="lg:w-1/2 w-full lg:mt-0 mt-8">
-                <div className="py-9 bg-[#f4f4f4] dark:bg-[#141414] rounded-[20px] border-b-3 border-r-3 border-red dark:border-none transition-colors duration-300">
+                <div className="py-9 bg-[#f4f4f4] dark:bg-common rounded-[20px] border-b-3 border-r-3 border-red dark:border-none transition-colors duration-300">
                   <h2 className="text-[48px] text-common dark:text-white font-bold font-inter leading-15.5 text-center">
                     200+
                   </h2>
@@ -62,11 +62,11 @@ const ExperienceCounter = () => {
               </div>
 
               <div className="lg:w-1/2 w-full lg:mt-0 mt-8">
-                <div className="py-9 bg-[#f4f4f4] dark:bg-[#141414] rounded-[20px] border-b-3 border-r-3 border-red dark:border-none transition-colors duration-300">
+                <div className="py-9 bg-[#f4f4f4] dark:bg-common rounded-[20px] border-b-3 border-r-3 border-red dark:border-none transition-colors duration-300">
                   <h2 className="text-[48px] text-common dark:text-white font-bold font-inter leading-15.5 text-center">
                     1,000+
                   </h2>
-                  <p className="text-base text-[#5D6570] dark:text-[#9F9F9F] font-normal font-rubik leading-6.5 mt-1 text-center">
+                  <p className="text-base text-[#5D6570] dark:text-[#9F9F9Ffix] font-normal font-rubik leading-6.5 mt-1 text-center">
                     our Satisfied Clientd
                   </p>
                 </div>
